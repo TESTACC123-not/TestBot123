68999bda72c39dfc893b84047a24e13e9b493215
TESTACC123-not <lukasscherr123@outlook.de>
2026-08-29 12:27:05 +0200

    Create Testijhbsaugihasdihbg.js

---
+++ Testijhbsaugihasdihbg.js
@@ -0,0 +1,3 @@
+// Testdatei – zeigt, dass der GitHub-Auto-Update auf dem Server funktioniert.
+console.log('[TEST] Server hat die Datei Testijhbsaugihasdihbg.js vom GitHub-Repo geholt.');
+console.log('Auto Update funktioniert! ' + new Date().toISOString());
